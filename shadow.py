ec2cd4f0cd19bb6118006a4ffe29c0b5f0ae5769
Intangere <clone60@gmail.com>
2022-04-21 14:41:55 -0400

    Create shadow.py

---
+++ shadow.py
@@ -0,0 +1,28 @@
+from PIL import Image, ImageFilter, ImageOps
+
+def dropShadow( image, shadow=(255,255,255,255), offset=(0,0), background = (0,0,0,255), iterations=3):
+
+  back = Image.open(sys.argv[1])
+
+  for x in range(0, back.width):
+      for y in range(0, back.height):
+          if back.getpixel((x,y))[3] != 0:
+             back.putpixel((x,y), shadow)
+          else:
+             back.putpixel((x,y), background)
+
+  n = 0
+  while n < iterations:
+    back = back.filter(ImageFilter.BoxBlur(15))
+    n += 1
+
+  back.paste(image, offset, image)
+  return back
+
+if __name__ == "__main__":
+  import sys
+
+  image = Image.open(sys.argv[1])
+
+  dropShadow(image, shadow=(234,100,100,255)).save('k.png')
+
